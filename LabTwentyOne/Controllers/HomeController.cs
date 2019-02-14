119f12a256b6e3a8951741b4be6248828844a1fc
Kent Butler <KentDavidButler@gmail.com>
2019-02-13 22:14:45 -0500

    Validation update!

--- LabTwentyOne/Controllers/HomeController.cs
+++ LabTwentyOne/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,14 +10,37 @@ namespace LabTwentyOne.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Welcome to the Frothy Clouds Coffee application. After registering, we'll be able to take your order.";
+
+            return View();
+        }
+
+        public ActionResult AddUser(User u)
+        {
+       
+            if(!(Regex.IsMatch(u.FirstName, @"^[a-zA-Z]+$")) || !(Regex.IsMatch(u.LastName, @"^[a-zA-Z]+$")))
+                {
+                return RedirectToAction("Register", new { message = "Error: In First or Last Name!" });
+                }
+            
+            if (!(Regex.IsMatch(u.Phone, (@"^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$"))))
+            {
+                return RedirectToAction("Register", new { message = "Error: Phone number is invalid!" });
+            }
+
+            if(u.Password != u.ConfirmPassword)
+            {
+                return RedirectToAction("Register", new { message = "Error: Passwords did not match" });
+            }
+
+            //use a viewbag similar to writeline when testing
+            ViewBag.Pronoun = u.Pronoun;
+            ViewBag.FirstName = u.FirstName;
+            ViewBag.LastName = u.LastName;
+            ViewBag.Phone = u.Phone;
+            ViewBag.Email = u.Email;
 
             return View();
         }
@@ -28,20 +52,20 @@ namespace LabTwentyOne.Controllers
             return View();
         }
 
-        public ActionResult Register()
+        public ActionResult Error()
         {
             return View();
         }
 
-        public ActionResult AddUser(User u)
+        public ActionResult Index()
         {
-            //use a viewbag similar to writeline when testing
-            ViewBag.Pronoun = u.Pronoun;
-            ViewBag.FirstName = u.FirstName;
-            ViewBag.LastName = u.LastName;
-            ViewBag.Phone = u.Phone;
-            ViewBag.Email = u.Email;
+            return View();
+        }
 
+
+        public ActionResult Register(string Message)
+        {
+            TempData["message"] = Message;
             return View();
         }
     }
--- LabTwentyOne/Models/User.cs
+++ LabTwentyOne/Models/User.cs
@@ -13,6 +13,7 @@ namespace LabTwentyOne.Models
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
 
     }
 }
